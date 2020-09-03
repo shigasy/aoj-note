bd849c8f12bafcbbc5575ae607b9c89e6412c665
shigasy <kxx555u.u@gmail.com>
2020-09-04 00:30:28 +0900

    add: 連結リストのNodeを動的に確保

--- src/ALDS1_3_C: Doubly Linked List/main.cpp
+++ src/ALDS1_3_C: Doubly Linked List/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
 using namespace std;
+struct Node {
+  int key;
+  // prev, nextはNode型の値のアドレスを持つことが出来るという宣言
+  Node *prev, *next;
+};
+
+// nilはNode型のアドレスを持つことが出来る
+Node *nil;
+
+// データを動的に変化させる必要があるデータ構造なため、必要な時にメモリを確保・解放する必要がある。
+// nextとprevを番兵に繋ぐことによって、空のリストを作る。番兵を作る理由は要素の削除・追加の実装が楽だから。空のリストは実データには含めない。
+void init() {
+  // 戻り値のポインタはvoid型で返ってくるので
+  // (Node *) でNode型ポインタにキャストしている
+  // mallocでヒープ領域にメモリを確保 malloc(size_t size)
+  // sizeは確保するバイトを指定する 今回はNode型のバイト数のメモリ領域を動的確保
+  nil = (Node *)malloc(sizeof(Node));
+  // -> はポインタ変数からメンバにアクセスするための演算子でアロー演算子と呼ぶ
+  nil->next = nil;
+  nil->prev = nil;
+}
 
 int main() {
   cout << "HelloWorld" << endl;
